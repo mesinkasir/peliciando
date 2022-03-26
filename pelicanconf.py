d338d29447a19fa1cfd9696fc8ccc41b6168a2a6
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2022-03-26 15:06:50 +0700

    Add files via upload

---
+++ pelicanconf.py
@@ -0,0 +1,54 @@
+AUTHOR = 'your name'
+SITENAME = 'Pelician Phyton Website Title'
+SITEURL = 'example.com'
+
+# inject a pelciando themes
+THEME = 'peliciando'
+
+PATH = 'content'
+
+# your time zone
+TIMEZONE = 'Asia/Jakarta'
+
+# web lang here
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Page menu for change page on web display
+PAGES = (
+('Home', 'https://example.com'),
+('About', 'https://example.com/about.html'),
+('Product', 'https://example.com/product.html'),
+('Order', 'https://example.com/order.html'),
+('Contact', 'https://example.com/contact.html'),
+)
+
+# Links area for change link menu in here
+LINKS = (('Phyton', 'https://www.python.org/'),
+('Pelician', 'https://blog.getpelican.com/'),
+('Peliciando', 'https://github.com/mesinkasir/peliciando'),
+('Axcora', 'https://axcora.com/getaxcoracms'),
+('Pico', 'https://axcora.com/picobook'),)
+
+# Social widget for insert your social menu here.
+SOCIAL = (('Facebook', 'https://fb.com/mesinkasircomplete'),
+('Instagram', 'https://instagram.com/mesinkasircomplete'),
+('Youtube', 'https://www.youtube.com/playlist?list=PLQDm6k9_HvYP5hlwkvNRYFColHFkyeRlM'),
+('Github', 'https://mesinkasir.github.io'),)
+
+# for contact page on website
+CONTACTS = (('Whatsapp', 'Link Wa here'),
+('Call Us', 'Phonelink here'),
+('Email', 'your email here'),
+('Map', 'Map Url here'),)
+
+# many article will displaying on home page website
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing #RELATIVE_URLS = True 
